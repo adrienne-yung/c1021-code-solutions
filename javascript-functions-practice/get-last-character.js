5b7a893947b6285d36c4fce5e5538a7567c569ef
Adrienne Yung <adrienneyung64@gmail.com>
2021-10-13 15:53:11 -0700

    Completed javascript-functions-practice exercise.

---
+++ javascript-functions-practice/get-last-character.js
@@ -0,0 +1,6 @@
+/* exported getLastCharacter */
+function getLastCharacter(string) {
+  var lastCharacterIndex = string.length - 1;
+  var lastCharacter = string[lastCharacterIndex];
+  return lastCharacter;
+}
