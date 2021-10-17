0afb37c818cee5d5d9cbb5703e66f24612ada319
Adrienne Yung <adrienneyung64@gmail.com>
2021-10-16 20:07:33 -0700

    Saved Changes to string-manipulation-1 exercise.

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,1 +1,9 @@
 /* exported getWords */
+/* input: receive group of words
+output: make group of words into a list
+step1: */
+
+function getWords(string) {
+  var withWords = string.split(' ');
+  return withWords;
+}
--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,1 +1,12 @@
 /* exported isVowel */
+/* input: letter
+output: true if letter is a vowel, false if letter is not a vowel
+step1: return value of "true" if value is a vowel, return value of "false" if letter is not a vowel. */
+
+function isVowel(character) {
+  if (character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u' || character === 'A' || character === 'E' || character === 'I' || character === 'O' || character === 'U') {
+    return true;
+  } else {
+    return false;
+  }
+}
