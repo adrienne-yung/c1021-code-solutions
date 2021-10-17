986280ea3d34555846aaae44d9f5842675a769e7
Adrienne Yung <adrienneyung64@gmail.com>
2021-10-16 23:43:10 -0700

    Completed string-manipulation-1 exercise.

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,9 +1,16 @@
 /* exported getWords */
 /* input: receive group of words
 output: make group of words into a list
-step1: */
+step1:  divide each group of words into a list of words
+step2: if there are no words in the supposed group of words, then don't return
+step3: return values */
 
 function getWords(string) {
-  var withWords = string.split(' ');
-  return withWords;
+  var array = string.split(' ');
+  if (string === '') {
+    return [];
+  } else {
+    array = string.split(' ');
+  }
+  return array;
 }
--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,10 +1,14 @@
 /* exported reverseWord */
 /* input: 'word'
 output: 'drow'
-step1: Create storage for output
-step2: Return value of output */
+step1: reverse letters in each word and store
+step2: join back each letter into a word
+step3: return the value of word */
 
 function reverseWord(word) {
-  var output = word.split('').reverse().join('');
-  return output;
+  var reverseWordResult = '';
+  for (var i = word.length - 1; i >= 0; i--) {
+    reverseWordResult += word[i];
+  }
+  return reverseWordResult;
 }
