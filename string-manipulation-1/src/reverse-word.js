409db648a839627a48a02bd333094f884721b7e3
Adrienne Yung <adrienneyung64@gmail.com>
2021-10-16 21:40:14 -0700

    saved changes tp string-manipulation-1 exercise.

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,1 +1,10 @@
 /* exported reverseWord */
+/* input: 'word'
+output: 'drow'
+step1: Create storage for output
+step2: Return value of output */
+
+function reverseWord(word) {
+  var output = word.split('').reverse().join('');
+  return output;
+}
