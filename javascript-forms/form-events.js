13417c08a7d607efea5833193218cd7394d8a569
Adrienne Yung <adrienneyung64@gmail.com>
2021-10-20 15:15:06 -0700

    Completed javascript-forms exercise.

---
+++ javascript-forms/form-events.js
@@ -0,0 +1,26 @@
+var userNameInput = document.querySelector('#user-name');
+userNameInput.addEventListener('focus', handleFocus);
+userNameInput.addEventListener('blur', handleBlur);
+userNameInput.addEventListener('input', handleInput);
+function handleFocus(event) {
+  console.log('focus event was fired');
+  console.log('event.target.name', event.target.name);
+}
+
+var userEmailInput = document.querySelector('#user-email');
+userEmailInput.addEventListener('blur', handleBlur);
+userEmailInput.addEventListener('focus', handleFocus);
+function handleBlur(event) {
+  console.log('blur event was fired');
+  console.log('event.target.name', event.target.name);
+}
+
+var userMessageInput = document.querySelector('#user-message');
+userMessageInput.addEventListener('input', handleInput);
+userMessageInput.addEventListener('focus', handleFocus);
+userMessageInput.addEventListener('blur', handleBlur);
+function handleInput(event) {
+  console.log('input event was fired');
+  console.log('event.target.name', event.target);
+  console.log('value of', event.target.name + ':', event.target.value);
+}
