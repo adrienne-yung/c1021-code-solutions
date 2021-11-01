107a44c202144d3ac6ae51678dcd88aaa8108709
Adrienne Yung <adrienneyung64@gmail.com>
2021-11-01 11:44:36 -0700

    Saved and worked on array-manipulation-1 exercise.

--- array-manipulation-1/src/last.js
+++ array-manipulation-1/src/last.js
@@ -2,7 +2,8 @@
 /* input: array
 output: last item of array
 step1: Create a storage space for the last item of an array
-step2: Return the value of storage space */
+step2: Create a storage space to access the last element
+step3: Return the value of storage space */
 
 function last(array) {
   var arrayIndex = array.length - 1;
--- array-manipulation-1/src/reverse.js
+++ array-manipulation-1/src/reverse.js
@@ -1,7 +1,13 @@
 /* exported reverse */
 /* input: array
-output: reverse display of array list
-step1: */
+output: reverse display of list
+step1: Create a storage space for reversed list
+step2: Create situation that the length of a list is not 0
+step3: Continue to pass through the list from the back if the
+        list length has not reached 0
+step4: if the list is empty, return empty
+step5: store values into storage space
+step6: return values */
 
 function reverse(array) {
   var reverseArrayResult = [];
