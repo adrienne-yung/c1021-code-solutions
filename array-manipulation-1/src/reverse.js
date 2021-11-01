aea391f39a369b8a240f9481c1c83562bfac5fe9
Adrienne Yung <adrienneyung64@gmail.com>
2021-11-01 11:42:55 -0700

    Worked on array-manipulation-1 exercise.

--- array-manipulation-1/src/reverse.js
+++ array-manipulation-1/src/reverse.js
@@ -5,13 +5,12 @@ step1: */
 
 function reverse(array) {
   var reverseArrayResult = [];
-  for (var i = array.length - 1; i >= 0; i--) {
-    reverseArrayResult += array[i];
-    if (array === []) {
-      return [];
-    } else {
-      reverseArrayResult = array.split(' ');
+  if (array.length !== 0) {
+    for (var i = array.length - 1; i >= 0; i--) {
+      reverseArrayResult += array[i];
     }
+  } else {
+    array = [];
   }
   return reverseArrayResult;
 }
--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -2,7 +2,12 @@
 /* input : list of words
 output: every word except for the first word
 step1: Create a storage space for any letter that is not the first letter of the list of words
-step2: return the value of storage space */
+step2: Create a situation in which from the back of the list of words
+step3: In the situation where you are at the position less than the length of the list of words
+       keep going.
+step4: give the result of returning every item but the first
+step5: if there is nothing in the list of words, return nothing
+step6: return the value of storage space */
 
 function tail(array) {
   var output = [];
