60a15782ef56c07650f36269f9377269ec29ab75
Adrienne Yung <adrienneyung64@gmail.com>
2021-11-02 20:02:24 -0700

    Saved changes to javascript-bank

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -31,13 +31,3 @@ Bank.prototype.getTotalAssets = function () {
   }
   return sumOfBankAssets;
 };
-
-/* Input: Given a possible client trying to open an account
-Output: Assign him an account / open account
-step1: Check if they got $ in the first place, else 'im wahhking here'
-step2: That money better be a whole number cause we a picky bank that doesn't accept cents
-step3: Create an account and grace client with an account number
-step4: Take all their money
-step5: Put that account and their money with the rest of the other future accounts
-step6: Thank you, next. Take the next account number +1
-step7: When done, holder and account details should be assigned to an account number and returned. */
