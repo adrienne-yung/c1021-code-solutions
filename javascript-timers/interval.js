495d35819ded5089aa3bd35a906f0391b7361ff7
Adrienne Yung <adrienneyung64@gmail.com>
2021-11-03 19:36:54 -0700

    Completed javascript-timers exercise.

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,38 +1,3 @@
-// var header = document.querySelector('h1');
-// function changeHeader() {
-//   if (header.textContent === 4) {
-//     header.textContent = 3;
-//   } else if (header.textContent === 3) {
-//     header.textContent = 2;
-//   } else if (header.textContent === 2) {
-//     header.textContent = 1;
-//   } else {
-//     header.textContent = 'Earth below us';
-//   }
-// }
-
-// setInterval(changeHeader, 1000);
-
-// var header = document.querySelector('h1');
-// var interval = setInterval(function () {
-//   if (header <= 4) {
-//     header.textContent = 'Earth Below Us';
-//     clearInterval(interval);
-//   }
-// }, 1000);
-
-// var header = document.querySelector('h1');
-// function countdown(seconds) {
-//   var counter = seconds;
-//   var interval = setInterval(() {
-//     counter--;
-//     if (counter < 0) {
-//       clearInterval(interval);
-//       header.textContent = "Earth Below Us"
-//     }
-//   }, 1000);
-// }
-
 var header = document.querySelector('h1');
 var seconds = 4;
 var timerId = null;
